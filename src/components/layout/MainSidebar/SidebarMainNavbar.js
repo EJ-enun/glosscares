8bb4875be8d83491bac2bc0ba00bfbf47f20b141
EJay Enun <68190790+EJ-enun@users.noreply.github.com>
2022-07-28 16:40:01 +0100

    Update SidebarMainNavbar.js

--- src/components/layout/MainSidebar/SidebarMainNavbar.js
+++ src/components/layout/MainSidebar/SidebarMainNavbar.js
@@ -35,7 +35,7 @@ class SidebarMainNavbar extends React.Component {
                 id="main-logo"
                 className="d-inline-block align-top mr-1"
                 style={{ maxWidth: "25px" }}
-                src={require("../../../images/logos/stethoscope.svg")}
+                src={require("../../../images/logos/ren.svg")}
                 alt="Glosscare Home"
               />
               {!hideLogoText && (
